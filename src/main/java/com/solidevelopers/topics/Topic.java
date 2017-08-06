4adc6df4e5e55defd4a56f0b8922301df3aaca16
arpit728 <agrawal.arpit35@gmail.com>
2017-08-06 16:46:15 +0530

    first commit

---
+++ src/main/java/com/solidevelopers/topics/Topic.java
@@ -0,0 +1,44 @@
+package com.solidevelopers.topics;
+
+/**
+ * Created by bugkiller on 6/10/2017.
+ */
+
+public class Topic {
+
+    private String id;
+    private String name,description;
+
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
